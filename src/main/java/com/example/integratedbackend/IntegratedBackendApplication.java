3c26915cbdd088e28ccc0e75350b6b7cf3e5f7f0
Tiskas02 <saksit.tati@mail.kmutt.ac.th>
2024-04-23 14:28:10 +0700

    create project

---
+++ src/main/java/com/example/integratedbackend/IntegratedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.integratedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegratedBackendApplication.class, args);
+    }
+
+}
