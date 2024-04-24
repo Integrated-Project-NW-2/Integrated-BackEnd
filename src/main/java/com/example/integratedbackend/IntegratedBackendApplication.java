f53a3bc853c919dffa438cdbb880a9d5c6a308dd
Tiskas02 <saksit.tati@mail.kmutt.ac.th>
2024-04-24 21:49:09 +0700

    add pbi-2

--- src/main/java/com/example/integratedbackend/IntegratedBackendApplication.java
+++ src/main/java/com/example/integratedbackend/IntegratedBackendApplication.java
@@ -1,13 +1,20 @@
 package com.example.integratedbackend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class IntegratedBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntegratedBackendApplication.class, args);
+    }
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
