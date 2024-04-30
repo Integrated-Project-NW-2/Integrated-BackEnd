a6e858e3aae8155fae3f5f26527e9eb77839a3b5
Jiraplus0304 <jiraplus.chan@mail.kmutt.ac.th>
2024-04-30 19:55:01 +0700

    updated code

--- src/main/java/com/example/integratedbackend/Controller/TaskController.java
+++ src/main/java/com/example/integratedbackend/Controller/TaskController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
-@CrossOrigin(origins = "http://localhost:4173")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TaskController {
     @Autowired
     TaskService service;
